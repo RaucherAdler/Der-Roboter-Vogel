0998bbeeef34c81203aba7c1085987f190054455
RaucherAdler <raucheradler@gmail.com>
2020-10-09 00:10:04 +0000

    HCF: pt. IV

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ async def removerole(ctx, member : discord.Member, role):
         await ctx.send(f'Rolle: {role} wurde vom {member.mention} entfernt!')
 
 @client.command(aliases=['help'])
-async def _help(self, * ctx):
+async def _help(self, ctx):
     helptext = "```"
     for command in self.bot.commands:
         helptext+=f"{command}\n"
