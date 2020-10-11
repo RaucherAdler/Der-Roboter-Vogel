e6bee379bbad8d6f298541c15cddf3d57e76f3de
RaucherAdler <raucheradler@gmail.com>
2020-10-11 14:18:43 +0000

    more unicode fixes?

--- main.py
+++ main.py
@@ -191,10 +191,10 @@ async def greet(ctx, member : discord.Member=None):
 
 @client.command(aliases=['geburtstag'])
 async def birthday(ctx, member : discord.Member):
-    tada = r'\u1F389'
+    tada = '\u1F389'
     await ctx.send(f'Alles gute zum geburtstag, {member.mention}! {tada}')
     await ctx.send('Jetzt singen wir alle das Geburtstagslied:')
-    cake = r'\u1F382'
+    cake = '\u1F382'
     embed_name = 'Geburtstagslied ' + cake
     embed_text = 'Zum Geburtstag viel Glück!\nZum Geburtstag viel Glück!\nZum Geburtstag liebe {name}\nZum Geburtstag viel Glück!'.format(member.mention)
     lyric_embed = discord.Embed(name=embed_name)
