1a76ed1ca1d6d126f65ca91e1a77e731899521a6
RaucherAdler <raucheradler@gmail.com>
2020-10-17 21:11:07 +0000

    implimented cogs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import discord.utils
 from discord import Color
-from pretty_help import PrettyHelp, Navigation
+from pretty_help import PrettyHelp
 from translate import Translator
 from datetime import datetime
 import pytz
@@ -12,14 +12,14 @@ intents = discord.Intents.default()
 intents.members = True
 
 
-client = commands.Bot(command_prefix = '/', intents=intents, help_command=PrettyHelp())
+client = commands.Bot(command_prefix = '/', intents=intents)
 
+client.help_command = PrettyHelp(color=Color.dark_red())
 
 @client.event
 async def on_ready():
    await client.change_presence(activity=discord.Activity(status=discord.Status.online, type=discord.ActivityType.playing, name='Your Mom'))
    print('Bot ist bereit!')
-
 
 @client.event
 async def on_member_join(member):
@@ -39,208 +39,208 @@ async def on_member_join(member):
         await member.add_roles(role)
         await channelname.send(f'{member.mention} wurde die Rolle gegeben: {role}!')
 
+class Moderation(commands.Cog):
+
+    @client.command(description='Clears a given number of messages', usage='`/clear <Number of posts>`')
+    @commands.has_permissions(manage_messages=True)
+    async def clear(ctx, amount=0):
+        await ctx.channel.purge(limit=amount+1)
 
 
-@client.command(description='Pings bots latency', usage='`/ping`')
-async def ping( ctx):
-    await ctx.send(f'Pong! `{round(client.latency * 1000)}ms`')
+    @client.command(description='Kicks a given user', usage='`/kick <Mention User>`')
+    @commands.has_permissions(kick_members=True)
+    async def kick(ctx, user: discord.Member, *, reason=None):
+        await ctx.send(f"{user} wurde getretten!")
+        await user.send(f'Sie wurden vom {ctx.message.author} vom {ctx.guild.name} getretten!')
+        if reason!= None:
+            await user.send(f'Grund: {reason}')
+        await user.kick(reason=reason)
 
 
-@client.command(description='Clears a given number of messages', usage='`/clear <Number of posts>`')
-@commands.has_permissions(manage_messages=True)
-async def clear(ctx, amount=0):
-    await ctx.channel.purge(limit=amount+1)
+    @client.command(description='Bans a given user', usage='`/ban <Mention User>`')
+    @commands.has_permissions(ban_members=True)
+    async def ban(ctx, user: discord.Member, *, reason=None):
+        await user.ban(reason=reason)
+        await ctx.send(f"{user} wurde verboten!")
+        await user.send(f'Sie wurden vom {ctx.message.author} vom {ctx.guild.name} gesperrt!')
+        if reason!= None:
+            await user.send(f'Grund: {reason}')
+        await user.ban(reason=reason)
 
 
-@client.command(description='Kicks a given user', usage='`/kick <Mention User>`')
-@commands.has_permissions(kick_members=True)
-async def kick(ctx, user: discord.Member, *, reason=None):
-    await ctx.send(f"{user} wurde getretten!")
-    await user.send(f'Sie wurden vom {ctx.message.author} vom {ctx.guild.name} getretten!')
-    if reason!= None:
-        await user.send(f'Grund: {reason}')
-    await user.kick(reason=reason)
+    @client.command(description='Unbans a given user', usage='`/unban <User Name (i.e. Raucher Adler#1220)>`')
+    @commands.has_permissions(ban_members=True)
+    async def unban(ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'{user.mention} wurde nicht verboten!')
 
 
-@client.command(description='Bans a given user', usage='`/ban <Mention User>`')
-@commands.has_permissions(ban_members=True)
-async def ban(ctx, user: discord.Member, *, reason=None):
-    await user.ban(reason=reason)
-    await ctx.send(f"{user} wurde verboten!")
-    await user.send(f'Sie wurden vom {ctx.message.author} vom {ctx.guild.name} gesperrt!')
-    if reason!= None:
-        await user.send(f'Grund: {reason}')
-    await user.ban(reason=reason)
+    @client.command(description='Unbans all banned users', usage='`/unbanall`')
+    @commands.has_permissions(ban_members=True)
+    async def unbanall(ctx):
+        banned_users = await ctx.guild.bans()
 
-
-@client.command(description='Unbans a given user', usage='`/unban <User Name (i.e. Raucher Adler#1220)>`')
-@commands.has_permissions(ban_members=True)
-async def unban(ctx, *, member):
-    banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('#')
-
-    for ban_entry in banned_users:
-        user = ban_entry.user
-
-        if (user.name, user.discriminator) == (member_name, member_discriminator):
+        for ban_entry in banned_users:
+            user = ban_entry.user
             await ctx.guild.unban(user)
             await ctx.send(f'{user.mention} wurde nicht verboten!')
 
 
-@client.command(description='Unbans all banned users', usage='`/unbanall`')
-@commands.has_permissions(ban_members=True)
-async def unbanall(ctx):
-    banned_users = await ctx.guild.bans()
-
-    for ban_entry in banned_users:
-        user = ban_entry.user
-        await ctx.guild.unban(user)
-        await ctx.send(f'{user.mention} wurde nicht verboten!')
-
-
-@client.command(description='a simple test command', usage='`/test`')
-async def test(ctx):
-    await ctx.send(f'Es vermisst nie.')
-
-
-@client.command(aliases=['translate'], description='Translate text (currently only supports German)', usage='`/translate <Message>`')
-async def _translate(ctx, message):
-    _translate
-    translator = Translator(to_lang="German")
-    translation = translator.translate(message)
-    await ctx.send(translation)
-
-
-@client.command(description='Gives role to a given user', usage='`/giverole <Mention User> <Role Name>`')
-@commands.has_permissions(manage_roles=True)
-async def giverole(ctx, member : discord.Member, role):
-    role = discord.utils.get(ctx.guild.roles, name=role)
-    if role == None:
-        await ctx.send('Diese Rolle existiert nicht! Bitte überprüfen Sie auf Tippfehler!')
-    else:
-        await member.add_roles(role)
-        await ctx.send(f'{member.mention} wurde die Rolle gegeben: {role}!')
-
-
-@client.command(description='Remvoes role from a given user', usage='`/removerole <Mention User> <Role Name>`')
-@commands.has_permissions(manage_roles=True)
-async def removerole(ctx, member : discord.Member, role):
-    role = discord.utils.get(ctx.guild.roles, name=role)
-    if role == None:
-        await ctx.send('Diese Rolle existiert nicht! Bitte überprüfen Sie auf Tippfehler!')
-    else:        
-        await member.remove_roles(role)
-        await ctx.send(f'Rolle: {role} wurde vom {member.mention} entfernt!')
-
-
-@client.command(description='Setup Command for automatic role assignment', usage='`/autorole <Default Role Name> <Main Channel Name>`')
-@commands.has_permissions(manage_roles=True)
-async def autorole(ctx, role, channel):
-    drole = discord.utils.get(ctx.guild.roles, name=role)
-    if drole == None:
-        await ctx.send('Diese Rolle existiert nicht! Bitte überprüfen Sie auf Tippfehler!')
-    else:  
-        defchannel = discord.utils.get(ctx.guild.channels, name='def-role')
-        if defchannel != None:
-            await defchannel.delete()      
-        overwrites = {ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),ctx.guild.me: discord.PermissionOverwrite(read_messages=True)}
-        newchannel = await ctx.guild.create_text_channel('def-role', overwrites=overwrites, topic='Auto generated channel by RoboterVogel, DO NOT delete or make any messages here.')   
-        await newchannel.send(f'{int(drole.id)}')
-    sendchannel = discord.utils.get(ctx.guild.channels, name=channel)
-    if sendchannel == None:
-            await ctx.send('Diese Kanal existiert nicht! Bitte überprüfen Sie auf Tippfehler!')
-    else:
-            await newchannel.send(f'{channel}')
-            await ctx.send(f'Neue Standardrolle ist {role}!')
-
-
-@client.command(aliases=['FCP'], description='Converts USD to FCP (Far Cry Primal)', usage='`/fcp <Amount of USD>`')
-async def fcp(ctx, amount):
-    server = ['this server', 'This Server', 'server', 'Server']
-    if amount in server:
-        FCP = 1
-        await ctx.send(f'`{amount}` ≈ `{FCP} FCP`')
-    else:    
-        amount = amount.replace('$', '')
-        usdtofcp = 1 / 30
-        FCP = float(usdtofcp) * float(amount)
-        await ctx.send(f'`{amount} USD` ≈ `{FCP} FCP`')
-
-
-@client.command(aliases=['USD'], description='Converts FCP (Far Cry Primal) to USD', usage='`/usd <Amount of FCP>`')
-async def usd(ctx, amount):
-    amount = amount.replace('FCPfcp', '')
-    fcptousd = 30
-    USD = float(fcptousd) * float(amount)
-    await ctx.send(f'`{amount} FCP` ≈ `{USD} USD`')
-
-@client.command(aliases=['hello', 'hallo', 'begruessung', 'begrüßung', 'greeting', 'gruessen', 'grüßen'], description='Greets user, or sends gretting to a different user', usage='`/greet < Mention User or "all" (optional)>`')
-async def greet(ctx, member : discord.Member=None):
-    tz_CDT = pytz.timezone('America/Chicago')
-    now_CDT = datetime.now(tz_CDT)
-    hour_CDT = now_CDT.hour
-    if hour_CDT in range(5, 10):
-        time_of_day = 'Morgen'
-        word_ending = 'en'
-    elif hour_CDT in range(11, 17):
-        time_of_day = 'Tag'
-        word_ending = 'en'
-    elif hour_CDT in range(18, 21):
-        time_of_day = 'Abend'
-        word_ending = 'en'
-    elif hour_CDT in range(22, 23) or range(0, 4):
-        time_of_day = 'Nacht'
-        word_ending = 'e'
-
-    good = 'Gut' + word_ending
-    author_id = ctx.message.author.id
-    if author_id == 755875742595678290 and member == None:
-        await ctx.send(f'{good} {time_of_day}, Vater!')
-    elif author_id != 755875742595678290 and member == None:
-        await ctx.send(f'{good} {time_of_day}, {ctx.message.author.mention}!')
-    if member != None:
-        if member == 'all':
-            await ctx.send(f'{good} {time_of_day}, @everyone!')
+    @client.command(description='Gives role to a given user', usage='`/giverole <Mention User> <Role Name>`')
+    @commands.has_permissions(manage_roles=True)
+    async def giverole(ctx, member : discord.Member, role):
+        role = discord.utils.get(ctx.guild.roles, name=role)
+        if role == None:
+            await ctx.send('Diese Rolle existiert nicht! Bitte überprüfen Sie auf Tippfehler!')
         else:
-            if member.id == 762768118212067328:
-                await ctx.send(f'Hallo {ctx.message.author.mention}, wie gehts?')
+            await member.add_roles(role)
+            await ctx.send(f'{member.mention} wurde die Rolle gegeben: {role}!')
+
+
+    @client.command(description='Remvoes role from a given user', usage='`/removerole <Mention User> <Role Name>`')
+    @commands.has_permissions(manage_roles=True)
+    async def removerole(ctx, member : discord.Member, role):
+        role = discord.utils.get(ctx.guild.roles, name=role)
+        if role == None:
+            await ctx.send('Diese Rolle existiert nicht! Bitte überprüfen Sie auf Tippfehler!')
+        else:        
+            await member.remove_roles(role)
+            await ctx.send(f'Rolle: {role} wurde vom {member.mention} entfernt!')
+
+
+    @client.command(description='Setup Command for automatic role assignment', usage='`/autorole <Default Role Name> <Main Channel Name>`')
+    @commands.has_permissions(manage_roles=True)
+    async def autorole(ctx, role, channel):
+        drole = discord.utils.get(ctx.guild.roles, name=role)
+        if drole == None:
+            await ctx.send('Diese Rolle existiert nicht! Bitte überprüfen Sie auf Tippfehler!')
+        else:  
+            defchannel = discord.utils.get(ctx.guild.channels, name='def-role')
+            if defchannel != None:
+                await defchannel.delete()      
+            overwrites = {ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),ctx.guild.me: discord.PermissionOverwrite(read_messages=True)}
+            newchannel = await ctx.guild.create_text_channel('def-role', overwrites=overwrites, topic='Auto generated channel by RoboterVogel, DO NOT delete or make any messages here.')   
+            await newchannel.send(f'{int(drole.id)}')
+        sendchannel = discord.utils.get(ctx.guild.channels, name=channel)
+        if sendchannel == None:
+                await ctx.send('Diese Kanal existiert nicht! Bitte überprüfen Sie auf Tippfehler!')
+        else:
+                await newchannel.send(f'{channel}')
+                await ctx.send(f'Neue Standardrolle ist {role}!')
+client.add_cog(Moderation(client))
+
+class Chat(commands.Cog):
+
+    @client.command(aliases=['hello', 'hallo', 'begruessung', 'begrüßung', 'greeting', 'gruessen', 'grüßen'], description='Greets user, or sends gretting to a different user', usage='`/greet < Mention User or "all" (optional)>`')
+    async def greet(ctx, member : discord.Member=None):
+        tz_CDT = pytz.timezone('America/Chicago')
+        now_CDT = datetime.now(tz_CDT)
+        hour_CDT = now_CDT.hour
+        if hour_CDT in range(5, 10):
+            time_of_day = 'Morgen'
+            word_ending = 'en'
+        elif hour_CDT in range(11, 17):
+            time_of_day = 'Tag'
+            word_ending = 'en'
+        elif hour_CDT in range(18, 21):
+            time_of_day = 'Abend'
+            word_ending = 'en'
+        elif hour_CDT in range(22, 23) or range(0, 4):
+            time_of_day = 'Nacht'
+            word_ending = 'e'
+
+        good = 'Gut' + word_ending
+        author_id = ctx.message.author.id
+        if author_id == 755875742595678290 and member == None:
+            await ctx.send(f'{good} {time_of_day}, Vater!')
+        elif author_id != 755875742595678290 and member == None:
+            await ctx.send(f'{good} {time_of_day}, {ctx.message.author.mention}!')
+        if member != None:
+            if member == 'all':
+                await ctx.send(f'{good} {time_of_day}, @everyone!')
             else:
-                await ctx.send(f'Grüße von {ctx.message.author.mention}, {member.mention}!')
+                if member.id == 762768118212067328:
+                    await ctx.send(f'Hallo {ctx.message.author.mention}, wie gehts?')
+                else:
+                    await ctx.send(f'Grüße von {ctx.message.author.mention}, {member.mention}!')
 
 
-@client.command(aliases=['geburtstag'], description='Sends birthday message for a user', usage='`/birthday <Mention User>`')
-async def birthday(ctx, member : discord.Member):
-    await ctx.send(f'Alles gute zum geburtstag, {member.mention}!  :tada:')
-    await ctx.send('Jetzt singen wir alle das Geburtstagslied:')
-    embed_name = 'Geburtstagslied :birthday:'
-    embed_text = 'Zum Geburtstag viel Glück!\nZum Geburtstag viel Glück!\nZum Geburtstag liebe {name}!\nZum Geburtstag viel Glück!'.format(name=member.mention)
-    lyric_embed = discord.Embed(name=embed_name)
-    lyric_embed.add_field(name=embed_name, value=embed_text)
-    await ctx.send(embed=lyric_embed)
+    @client.command(aliases=['geburtstag'], description='Sends birthday message for a user', usage='`/birthday <Mention User>`')
+    async def birthday(ctx, member : discord.Member):
+        await ctx.send(f'Alles gute zum geburtstag, {member.mention}!  :tada:')
+        await ctx.send('Jetzt singen wir alle das Geburtstagslied:')
+        embed_name = 'Geburtstagslied :birthday:'
+        embed_text = 'Zum Geburtstag viel Glück!\nZum Geburtstag viel Glück!\nZum Geburtstag liebe {name}!\nZum Geburtstag viel Glück!'.format(name=member.mention)
+        lyric_embed = discord.Embed(name=embed_name)
+        lyric_embed.add_field(name=embed_name, value=embed_text)
+        await ctx.send(embed=lyric_embed)
 
-"""    
-@client.command(description='Join Voice Channel, ', usage='`/join`')
-async def join(ctx):
-    member = ctx.message.author
-    voice_channel = member.voice.channel
-    if voice_channel != None:
-        await ctx.send(f'Jetzt `{voice_channel}` eingeben!')
-        await voice_channel.connect()
-    else:
-        await ctx.send(f'Sie befinden sich nicht in einem Sprachkanal!')
+    @client.command(description='Pings bots latency', usage='`/ping`')
+    async def ping(ctx):
+        await ctx.send(f'Pong! `{round(client.latency * 1000)}ms`')    
+client.add_cog(Chat(client))
+
+class Conversion(commands.Cog):
+
+    @client.command(aliases=['translate'], description='Translate text (currently only supports German)', usage='`/translate <Message>`')
+    async def _translate(ctx, message):
+        translator = Translator(to_lang="German")
+        translation = translator.translate(message)
+        await ctx.send(translation)
+
+    @client.command(aliases=['FCP'], description='Converts USD to FCP (Far Cry Primal)', usage='`/fcp <Amount of USD>`')
+    async def fcp(ctx, amount):
+        server = ['this server', 'This Server', 'server', 'Server']
+        if amount in server:
+            FCP = 1
+            await ctx.send(f'`{amount}` ≈ `{FCP} FCP`')
+        else:    
+            amount = amount.replace('$', '')
+            usdtofcp = 1 / 30
+            FCP = float(usdtofcp) * float(amount)
+            await ctx.send(f'`{amount} USD` ≈ `{FCP} FCP`')
 
 
-@client.command(description='Leave Voice Channel', usage='`/leave`')
-async def leave(ctx):
-    member = ctx.message.author
-    voice_channel = member.voice.channel
-    vc = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
-    if voice_channel and vc != None:
-        await vc.disconnect()
-        await ctx.send(f'Auf Wiedersehen!')
-    else:
-        await ctx.send(f'Derzeit nicht in Sprachkanal!')
-"""
+    @client.command(aliases=['USD'], description='Converts FCP (Far Cry Primal) to USD', usage='`/usd <Amount of FCP>`')
+    async def usd(ctx, amount):
+        amount = amount.replace('FCPfcp', '')
+        fcptousd = 30
+        USD = float(fcptousd) * float(amount)
+        await ctx.send(f'`{amount} FCP` ≈ `{USD} USD`')
+client.add_cog(Conversion(client))
+
+class Voice(commands.Cog):
+
+    @client.command(description='Join Voice Channel, ', usage='`/join`')
+    async def join(ctx):
+        member = ctx.message.author
+        voice_channel = member.voice.channel
+        if voice_channel != None:
+            await ctx.send(f'Jetzt `{voice_channel}` eingeben!')
+            await voice_channel.connect()
+        else:
+            await ctx.send(f'Sie befinden sich nicht in einem Sprachkanal!')
+
+
+    @client.command(description='Leave Voice Channel', usage='`/leave`')
+    async def leave(ctx):
+        member = ctx.message.author
+        voice_channel = member.voice.channel
+        vc = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+        if voice_channel and vc != None:
+            await vc.disconnect()
+            await ctx.send(f'Auf Wiedersehen!')
+        else:
+            await ctx.send(f'Derzeit nicht in Sprachkanal!')
+client.add_cog(Voice(client))
+
 
 key = 'NzYyNzY4MTE4MjEyMDY3MzI4.X3t9Kg.pLG6YLPVdbNqL9FI1iijx3YJ4T4'
 client.run(key)
