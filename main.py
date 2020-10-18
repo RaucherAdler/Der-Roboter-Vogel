bd3f53931c5cd9095509931036486bb2cb959c07
RaucherAdler <larrykillips@gmail.com>
2020-10-18 01:23:34 +0000

    await for invocation

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def on_guild_join(guild):
     owner = guild.owner
     info = client.get_command('info')
     owner_dmchannel = owner.dm_channel
-    info_embed = info.invoke(owner_dmchannel)
+    info_embed = await info.invoke(owner_dmchannel)
     await owner.send(f'Hallo, ich bin RoboterVogel, dein neuer Bot!', embed=info_embed)
 
 class Moderation(commands.Cog):
