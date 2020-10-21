858c361421fab2241bace05dc3a6cc0021ea7bad
RaucherAdler <larrykillips@gmail.com>
2020-10-21 19:32:23 +0000

    added check for bot in ban/kick commands

--- main.py
+++ main.py
@@ -66,21 +66,26 @@ class Moderation(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def kick(ctx, user: discord.Member, *, reason=None):
         await ctx.send(f"{user} wurde getretten!")
-        await user.send(f'Sie wurden vom {ctx.message.author} vom {ctx.guild.name} getretten!')
-        if reason!= None:
-            await user.send(f'Grund: {reason}')
-        await user.kick(reason=reason)
+        if user.bot == True:
+            await user.kick(reason=reason)
+        else:
+            await user.send(f'Sie wurden vom {ctx.message.author} vom {ctx.guild.name} getretten!')
+            if reason!= None:
+                await user.send(f'Grund: {reason}')
+            await user.kick(reason=reason)
 
 
     @client.command(description='Bans a given user', usage='`/ban <Mention User>`')
     @commands.has_permissions(ban_members=True)
     async def ban(ctx, user: discord.Member, *, reason=None):
-        await user.ban(reason=reason)
         await ctx.send(f"{user} wurde verboten!")
-        await user.send(f'Sie wurden vom {ctx.message.author} vom {ctx.guild.name} gesperrt!')
-        if reason!= None:
-            await user.send(f'Grund: {reason}')
-        await user.ban(reason=reason)
+        if user.bot == True:
+            await user.ban(reason=reason)
+        else:
+            await user.send(f'Sie wurden vom {ctx.message.author} vom {ctx.guild.name} gesperrt!')
+            if reason!= None:
+                await user.send(f'Grund: {reason}')
+            await user.ban(reason=reason)
 
 
     @client.command(description='Unbans a given user', usage='`/unban <User Name (i.e. Raucher Adler#1220)>`')
