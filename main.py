fa0b99c938cf3d42ceb1fd3a937265f59ca33f3e
RaucherAdler <raucheradler@gmail.com>
2020-10-31 01:37:54 +0000

    operand fix?

--- main.py
+++ main.py
@@ -217,18 +217,21 @@ class Moderation(commands.Cog):
     async def stats(ctx):
         stats_embed = discord.Embed(name='Guild Stats', color=Color.dark_red())
         members = ctx.guild.members
+        online_member_number = 0
+        total_member_number = 0
+        bot_number = 0
         for member in members:
             if member.status != 'offline':
                 if member.bot == False:
-                    online_member_number =+ 1
-                    total_member_number =+ 1
+                    online_member_number += 1
+                    total_member_number += 1
                 else:
-                    bot_number =+ 1
+                    bot_number += 1
             elif member.status == 'offline':
                 if member.bot == False:
-                    total_member_number =+ 1
+                    total_member_number += 1
                 else:
-                    bot_number =+ 1
+                    bot_number += 1
         stats_embed.add_field(name='Active Members: ', value=f'{online_member_number}', inline=True)
         stats_embed.add_field(name='Total Members: ', value=f'{total_member_number}', inline=True)
         stats_embed.add_field(name='Bots: ', value=f'{bot_number}', inline=True)
