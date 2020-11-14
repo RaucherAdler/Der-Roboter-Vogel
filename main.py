4106d4876a5597184a963a6316b509eb1fca86e0
RaucherAdler <raucheradler@gmail.com>
2020-11-14 22:10:05 +0000

    possible fix for iterable in remove role?

--- main.py
+++ main.py
@@ -297,7 +297,7 @@ class Chat(commands.Cog):
         lyric_embed.add_field(name=embed_name, value=embed_text)
         lyric_embed.set_footer(text=member, icon_url=member.avatar_url)
         await ctx.send(embed=lyric_embed)
-        scheduler.enter(60, 1, member.remove_roles, (bday_role))
+        scheduler.enter(60, 1, member.remove_roles, (bday_role,))
         scheduler.run()
 
 
