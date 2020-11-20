5f0c68031427ca4438013c0b2492be2123a5d6c5
RaucherAdler <raucheradler@gmail.com>
2020-11-20 15:43:11 +0000

    removed dict from find_all

--- main.py
+++ main.py
@@ -525,7 +525,7 @@ class Voice(commands.Cog):
             r = requests.get(query_link)
             page = r.text
             soup = BeautifulSoup(page, 'html.parser')
-            videos = soup.find_all('a', {'id': "video-title"})
+            videos = soup.find_all('a')
             video_list = []
             for v in videos:
                 if '/watch?' in v['href']:
