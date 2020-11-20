8b89894fb462c6a80ee2a68f584e20a134f7d212
RaucherAdler <raucheradler@gmail.com>
2020-11-20 05:15:23 +0000

    possible filename fix

--- main.py
+++ main.py
@@ -518,7 +518,7 @@ class Voice(commands.Cog):
             else:
                 await ctx.send(f'Jetzt `{member_voice_channel}` eingeben!')
                 await member_voice_channel.connect()
-        ydl_opts = {'postprocessors': [{'key': 'FFmpegExtractAudio','preferredcodec': 'mp3','preferredquality': '192', 'outtmpl': 'song.mp3'}]}
+        ydl_opts = {'postprocessors': [{'key': 'FFmpegExtractAudio','preferredcodec': 'mp3','preferredquality': '192','outtmpl': 'song.mp3'}]}
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             #for video in ydl.extract_info(song, download=False):
                 #for property in ['id', 'title', 'duration']:
@@ -530,9 +530,13 @@ class Voice(commands.Cog):
                         #filename = f'{video.get(property)}.mp3'
             #song_embed.set_footer(text=ctx.message.author, icon_url=ctx.message.author.avatar_url)
             #await ctx.send(embed=song_embed)
+            attr_dict = ydl.extract_info(song, download=False)
+            title = attr_dict.values('title')
+            id = attr_dict.values('id')
+            filename = title + id +'.mp3'
             await ctx.send('Downloading...')
             ydl.download([song])
-            source = discord.FFmpegOpusAudio('song.mp3', executable='ffmpeg')
+            source = discord.FFmpegOpusAudio(source=filename, executable='ffmpeg')
             current_VoiceClient = discord.utils.get(client.voice_clients, guild=ctx.guild)
             await ctx.send('Spielen:')
             while not current_VoiceClient.play(source):
