87df61364ed84553cf9c993ad786b8a320b0e873
RaucherAdler <raucheradler@gmail.com>
2020-11-22 18:46:47 -0600

    passing in dict, probably still broke :(

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def add_to_queue(guild_id, attributes):
     g_coll = collection[f"{guild_id}"]
     entries = g_coll["entries"] 
     json_attrs = json.dumps(attributes)
-    entries.insert_one(json_attrs)
+    entries.insert_one(attributes)
     position = entries.count()
     return position
 
