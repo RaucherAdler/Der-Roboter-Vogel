2102ef590686a2c6a62161e8ba6c64b86dfd0efe
RaucherAdler <raucheradler@gmail.com>
2020-12-01 12:26:39 -0600

    forgot actuall check for if an error is raised, haha, and it still doesn't work, what'd you expect?

--- main.py
+++ main.py
@@ -692,7 +692,8 @@ class Music(commands.Cog):
     def _handle_queue(self, error=None):
         ctx = Music.context
         loop = client.loop
-        asyncio.run_coroutine_threadsafe(ctx.send('Es ist ein Fehler aufgetreten!'), loop)
+        if error != None:
+            asyncio.run_coroutine_threadsafe(ctx.send('Es ist ein Fehler aufgetreten!'), loop)
         guild_id = ctx.guild.id
         g_coll = collection[f"{guild_id}"]
         np_coll = g_coll["now_playing"]
