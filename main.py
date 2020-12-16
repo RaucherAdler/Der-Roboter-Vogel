d910bdf323d9889678ee39f013f65aae48ecb567
RaucherAdler <raucheradler@gmail.com>
2020-12-16 01:42:49 -0600

    updated args so that member should be None by default, otherwise should be Member type

--- main.py
+++ main.py
@@ -312,8 +312,8 @@ class Moderation(commands.Cog):
 
 
     @client.command(aliases=['Profile', 'whois', 'Whois'], description='Fetches user info such as account age, name, etc.', usage='/profile <Mention Member (Optional)>')
-    async def profile(ctx, member=None):
-        if isinstance(member, discord.Member) == False or isinstance(member, discord.User) == False:
+    async def profile(ctx, member : discord.Member=None):
+        if member == None:
             member = ctx.message.author
         profile_embed = discord.Embed(title=f'User — {member}', color=Color.dark_red())
         profile_embed.set_thumbnail(url=member.avatar_url)
