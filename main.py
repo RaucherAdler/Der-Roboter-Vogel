1b93a5d3e6e7443ea5705129cd10c7b5140daac6
RaucherAdler <raucheradler@gmail.com>
2020-12-16 01:28:58 -0600

    seems way I used for checking type isn't working or something

--- main.py
+++ main.py
@@ -313,7 +313,7 @@ class Moderation(commands.Cog):
 
     @client.command(aliases=['Profile', 'whois', 'Whois'], description='Fetches user info such as account age, name, etc.', usage='/profile <Mention Member (Optional)>')
     async def profile(ctx, member=None):
-        if isinstance(member, discord.Member) == False:
+        if isinstance(member, discord.Member) == False or isinstance(member, discord.User) == False:
             member = ctx.message.author
         profile_embed = discord.Embed(title=f'User — {member}', color=Color.dark_red())
         profile_embed.set_thumbnail(url=member.avatar_url)
